c2bf6804e30e0914fdf41323656c15c4ccb5926d
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2026-06-30 15:54:25 -0300

    plan estandarizacion visual. fase 16

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { routes } from './app/app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
-import { NgxSpinnerModule } from 'ngx-spinner';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { ToastrModule } from 'ngx-toastr';
 import { isDevMode, importProvidersFrom } from '@angular/core';
@@ -18,7 +17,7 @@ bootstrapApplication(AppComponent, {
     providers: [
         provideAnimations(),
         provideRouter(routes),
-        importProvidersFrom(FormsModule, ReactiveFormsModule, NgxPaginationModule, CommonModule, NgxSpinnerModule.forRoot({ type: 'ball-spin-clockwise-fade-rotating' }), ServiceWorkerModule.register('ngsw-worker.js', {
+        importProvidersFrom(FormsModule, ReactiveFormsModule, NgxPaginationModule, CommonModule, ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: !isDevMode(),
             // Register the ServiceWorker as soon as the application is stable
             // or after 30 seconds (whichever comes first).
